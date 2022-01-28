7389e6add782bc90fe9717db09baced48546e153
Ronald <ronalddavisr3@gmail.com>
2022-01-28 15:37:58 -0500

    changed docker-compose config a bit

--- gourmandapiapp/main.py
+++ gourmandapiapp/main.py
@@ -14,7 +14,7 @@ app.include_router(auth.router)
 
 @app.get("/")
 def home():
-    return {"detail": "Feel free to check of the documentations at...",
+    return {"detail": "Feel free to check of the documentation at...",
             "option 1": "https://www.raindata.xyz/docs",
             "option 2": "https://www.raindata.xyz/redocs",
-            "news": "CI/CD Pipeline is functioning now and the docker volume 😀"}
+            "news": "CI/CD Pipeline is functioning now through docker image push to docker hub and then docker-compose run on remote machine "}
