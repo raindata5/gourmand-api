53055c2e3afec68bdb40607a052083560d7ecb81
Ronald <ronalddavisr3@gmail.com>
2023-10-02 22:25:10 -0400

    use argon2 kdf for initial login

--- gourmandapiapp/utils.py
+++ gourmandapiapp/utils.py
@@ -1,8 +1,23 @@
 from passlib.context import CryptContext
+from passlib.hash import argon2
+from passlib.exc import UnknownHashError
+import secrets
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+interface_argon2 = lambda the_salt: argon2.using(salt_size=16, rounds=2, salt=the_salt)
+
 def get_password_hash(password):
-    return pwd_context.hash(password)
+    # TODO: encode in UTF8 bytes
+    salt = secrets.token_bytes(16)
+    return  interface_argon2(the_salt=salt).hash(password)
 
 def verification(plain_password, hashed_password):
-    return pwd_context.verify(plain_password,hashed_password)
+    salt = secrets.token_bytes(16)
+    for algo in [interface_argon2(the_salt=salt), pwd_context]:
+        try:
+            if algo.verify(plain_password,hashed_password):
+                return True
+        except (UnknownHashError, ValueError):
+            print('Reverting to bcrypto algo')
+            continue
+    return False
--- gourmandapiapptests/unit/test_crypto.py
+++ gourmandapiapptests/unit/test_crypto.py
@@ -1,6 +1,8 @@
 import pytest
-from gourmandapiapp import models
-
+from gourmandapiapp import (
+    models,
+    utils
+)
 
 def test_pass_hash_verified():
     user = models.AuthUserModelORM(
@@ -23,3 +25,15 @@ def test_pass_hash_unsuccessful():
     user.passy = 'natalia'
     assert not user.check_password('natalia12345')
     print(vars(user))
+
+def test_verification_pass():
+    message = 'ronald'
+    message_hash = utils.get_password_hash(message)
+    assert utils.verification(message, message_hash)
+
+def test_verification_fail():
+    message = 'ronald'
+    message_false = 'ronald123'
+    message_hash = utils.get_password_hash(message)
+    # with pytest.raises(utils.UnknownHashError):
+    assert not utils.verification(message_false, message_hash)
