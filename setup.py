dc52a5a215be034315613b90b70a946966d5842a
Ronald <ronalddavisr3@gmail.com>
2023-10-09 22:49:14 -0400

    add some helper commands to setup.py; use utility from bootstrap in html blockquote

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
 from setuptools import setup
 
 setup()
+# docker build --tag raindata5/gourmand-api .
+# docker images --quiet --filter=dangling=true | xargs --no-run-if-empty docker rmi
+# docker push raindata5/gourmand-api
